dd779ef6db6770245f9e685cc84a375c3fc584a2
nikhxxt <nikhxtwork@gmail.com>
2025-10-26 21:14:00 +0530

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,13 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
