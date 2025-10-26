c4e27152620e31cf206ce98a3166169f8e9285d1
nikhxxt <nikhxtwork@gmail.com>
2025-10-26 20:48:22 +0530

    Update employee.py

--- app/models/employee.py
+++ app/models/employee.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, String, Integer, Date, ForeignKey
+from sqlalchemy import Column, String, Integer, Date
 from app.db.base import Base
 
 class Employee(Base):
     __tablename__ = "employees"
+
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String, unique=True, index=True)
-    role = Column(String)
-    department = Column(String)
-    date_joined = Column(Date)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    role = Column(String, nullable=False)
+    department = Column(String, nullable=False)
+    date_joined = Column(Date, nullable=False)
