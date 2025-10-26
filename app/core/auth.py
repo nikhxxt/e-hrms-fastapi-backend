274bc9b7154a7ca98db62c643cf731a460db01b0
nikhxxt <nikhxtwork@gmail.com>
2025-10-26 20:13:07 +0530

    Create auth.py

---
+++ app/core/auth.py
@@ -0,0 +1,13 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from app.schemas.auth import TokenData
+
+SECRET_KEY = "your_jwt_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
